787e9f6f68440e4c9d5d46177c16c0e2eca82a63
ClemensElflein <clemens1@familie-elflein.de>
2017-05-01 14:23:48 +0200

    1st fully working joystick implementation. Tried with several sims

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,13 +1,24 @@
+#include "Joystick.h"
+
+// Create Joystick
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD, 0,
+    0,true, true, false, false, false, false, true, true, false, false, false);
+    
 long lastReceived = 0;
 byte data[256];
 uint8_t recPos = 0;
 int16_t channels[256];
 
 void setup() {
-  Serial.begin(100000);
   Serial1.begin(115200);
   // Make it invalid
   data[0] = 0;
+
+  Joystick.begin(false);
+  Joystick.setXAxisRange(0, 15000);
+  Joystick.setYAxisRange(0, 15000);
+  Joystick.setRudderRange(0, 15000);
+  Joystick.setThrottleRange(0, 15000);
 }
 
 void loop() {
@@ -31,6 +42,15 @@ byte parseSumd() {
   byte count = data[2];
   for(byte c = 0; c < count; c++)
     channels[c] = ((int16_t)data[3+c*2]) << 8 | data[4+c*2];
+
+  Joystick.setXAxis(channels[1]);
+  Joystick.setYAxis(channels[2]);
+  Joystick.setRudder(channels[3]);
+  Joystick.setThrottle(channels[0]);
+  Joystick.sendState();
+
+
+  delay(10);
   
   return count;
 }
